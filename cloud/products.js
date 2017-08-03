d425ad3e7afe72b659e895fd9e7abf65dca09f63
Jeremy Adam <male@jeremyadam.com>
2017-08-03 13:25:49 -0500

    reduce updateAwaitingInventoryQueue logging

--- cloud/products.js
+++ cloud/products.js
@@ -1885,7 +1885,7 @@ Parse.Cloud.define("updateAwaitingInventoryQueue", function(request, response) {
         var variants = orderProduct.has('editedVariants') ? orderProduct.get('editedVariants') : orderProduct.has('variants') ? orderProduct.get('variants') : [];
         _.each(variants, function(variant) {          
           awaitingInventory = awaitingInventory.map(function(item) {
-            if (orderProduct.get('order_id') === 8751) logInfo(item.object.get('variant').id + ':' + variant.id)
+//             if (orderProduct.get('order_id') === 8751) logInfo(item.object.get('variant').id + ':' + variant.id)
             if (item.object.get('variant').id == variant.id && item.available > 0) {
               var numNeeded = orderProduct.get('quantity') - orderProduct.get('quantity_shipped');
               if (variant.get('inventoryLevel') > 0) numNeeded - variant.get('inventoryLevel');
@@ -1902,20 +1902,20 @@ Parse.Cloud.define("updateAwaitingInventoryQueue", function(request, response) {
               logInfo((item.vendorOrder ? 'Vendor Order '+item.vendorOrder.get('vendorOrderNumber') : 'Resize') + ' matched variant ' + variant.get('variantId') + '. In stock ' + variant.get('inventoryLevel') + ', need ' + numNeeded + ', available ' + item.available + ', reserved ' + item.reserved);
               if (numNeeded > 0 && item.available >= numNeeded) {
                 var numToSubtract = numNeeded < item.available ? numNeeded : item.available;
-                if (orderProduct.get('order_id') === 8751) logInfo('Subtract ' + numToSubtract + ' for ' + variant.get('variantId'));
+//                 if (orderProduct.get('order_id') === 8751) logInfo('Subtract ' + numToSubtract + ' for ' + variant.get('variantId'));
                 item.available -= numToSubtract;
                 item.reserved += numToSubtract;
                 orderProductReservations.push({orderProductId: orderProduct.get('orderProductId'), numReserved: numToSubtract});
-                if (orderProduct.get('order_id') === 8751) logInfo(item.available + ' now available for ' + variant.get('variantId'));
-                if (orderProduct.get('order_id') === 8751) logInfo(item.reserved + ' now reserved for ' + variant.get('variantId'));
-                if (orderProduct.get('order_id') === 8751) logInfo('set awaiting inventory ' + variant.get('variantId') + ' for order ' + orderProduct.get('order_id'));
+//                 if (orderProduct.get('order_id') === 8751) logInfo(item.available + ' now available for ' + variant.get('variantId'));
+//                 if (orderProduct.get('order_id') === 8751) logInfo(item.reserved + ' now reserved for ' + variant.get('variantId'));
+//                 if (orderProduct.get('order_id') === 8751) logInfo('set awaiting inventory ' + variant.get('variantId') + ' for order ' + orderProduct.get('order_id'));
                 orderProductAwaitingInventory.push(item.object);
                 if (item.vendorOrder) {
 //                   logInfo('set awaiting inventory vendor order ' + item.vendorOrder.get('vendorOrderNumber') + ' for order ' + orderProduct.get('order_id'));
                   orderProductAwaitingInventoryVendorOrders.push(item.vendorOrder);
                 }
               } else {
-                if (orderProduct.get('order_id') === 8751) logInfo('No more available for ' + variant.get('variantId'));
+//                 if (orderProduct.get('order_id') === 8751) logInfo('No more available for ' + variant.get('variantId'));
               }
             } 
             return item;
@@ -2455,7 +2455,12 @@ Parse.Cloud.afterSave("Product", function(request) {
       items.push(order.get('orderId'));
     });
     
-    return Parse.Cloud.run('addToReloadQueue', {objectClass: 'Order', items: items});
+    if (items.length > 0) {
+      return Parse.Cloud.run('addToReloadQueue', {objectClass: 'Order', items: items});
+    } else {
+      return true;
+    }
+    
     
   }).then(function(result) {
     logInfo('Product afterSave success for product ' + productId);
