f57f0f5655aff75fed0ab61dc3489745ae5448f5
edisonpaul4 <edisonpaul4@gmail.com>
2018-06-01 09:31:45 -0500

    Stats only from January 2018

--- cloud/stats/stats.controller.js
+++ cloud/stats/stats.controller.js
@@ -1,6 +1,7 @@
 const { ProductsModel } = require('../products/products.model');
 const { ReturnsModel } = require('../returns/returns.model');
 const { OrdersModel } = require('../orders/orders.model');
+var _ = require('underscore');
 
 var moment = require('moment-timezone');
 
@@ -13,33 +14,39 @@ exports.StatsController = new class StatsCrontroller {
     var response = ProductsModel.getProductsByFilters({
       limit: 10000
     }).find().then(products => {
-      return OrdersModel.getOrderProductsByFilters(
-        { limit: 10000,greaterOrEqual: [{ key: 'createdAt', value: moment('20180101').toDate() }] }).find().then(productOrders => {
-          console.log('done order product')
-          return ReturnsModel.getReturnsByFilters( { limit: 10000, greaterOrEqual: [{ key: 'createdAt', value: moment('20180101').toDate() }] }).find().then(productReturns =>{
-            console.log('done return')
-            return products.map(product => this.createProductStatsObject(product, productOrders, productReturns))
-          })
-        });
+      //return OrdersModel.getOrderProductsByFilters(
+      //  { limit: 10000,greaterOrEqual: [{ key: 'createdAt', value: moment('20180128').toDate() }] }).find().then(productOrders => {
+      console.log('done order product')
+      return ReturnsModel.getReturnsByFilters({ limit: 100000, greaterOrEqual: [{ key: 'createdAt', value: moment('20180101').toDate() }] }).find().then(productReturns => {
+        console.log('done return')
+        return Promise.all(products.map(product => this.createProductStatsObject(product, null, productReturns)))
+      })
+      //  });
     });
-    return response;
+    return (response);
   }
 
   createProductStatsObject(productObject, productOrders, productReturns) {
     const unitsRepaired = typeof productObject.get('totalRepaired') !== 'undefined' ? productObject.get('totalRepaired') : 0;
-    var countingSold =productOrders.filter(result => result.get('product_id') == productObject.get('productId')).length;
+    var countingSold = 0;
     var countingReturn = productReturns.filter(result => result.get('productId') == productObject.get('productId')).length;
-    //productOrders.filter(result => result.get('product_id') == productObject.get('productId')).map(result => countingSold += result.get('quantity'))
-    return {
-      productId: productObject.get('productId'),
-      productName: productObject.get('name'),
-      totalSold: countingSold,
-      unitsReturned: countingReturn,
-      unitsReturnedP: countingSold !== 0 ? (countingReturn * 100) / countingSold : 0,
-      unitsRepaired,
-      unitsRepairedP: countingSold !== 0 ? (unitsRepaired * 100) / countingSold : 0,
-      totalReveneu: productObject.get('price') * countingSold
-    };
+    // _.filter(productOrders,result => result.get('product_id') == productObject.get('productId')).map(result => countingSold += result.get('quantity'))
+
+    return OrdersModel.getOrderProductsByFilters(
+      { limit: 10000, equal: [{ key: 'product_id', value: productObject.get('productId') }], greaterOrEqual: [{ key: 'createdAt', value: moment('20180101').toDate() }] }).find().then(productOrders => {
+        productOrders.map(result => countingSold += result.get('quantity'))
+        return {
+          productId: productObject.get('productId'),
+          productName: productObject.get('name'),
+          totalSold: countingSold,
+          unitsReturned: countingReturn,
+          unitsReturnedP: countingSold !== 0 ? (countingReturn * 100) / countingSold : 0,
+          unitsRepaired,
+          unitsRepairedP: countingSold !== 0 ? (unitsRepaired * 100) / countingSold : 0,
+          totalReveneu: productObject.get('price') * countingSold
+        };
+      })
+
 
 
   }
