fb7c0f5cec28aeb9b2f66d15fa3f713795d079c3
edisonpaul4 <edisonpaul4@gmail.com>
2018-06-01 09:41:21 -0500

    Fixed stats with returns types

--- cloud/stats/stats.controller.js
+++ cloud/stats/stats.controller.js
@@ -17,7 +17,7 @@ exports.StatsController = new class StatsCrontroller {
       //return OrdersModel.getOrderProductsByFilters(
       //  { limit: 10000,greaterOrEqual: [{ key: 'createdAt', value: moment('20180128').toDate() }] }).find().then(productOrders => {
       console.log('done order product')
-      return ReturnsModel.getReturnsByFilters({ limit: 100000, greaterOrEqual: [{ key: 'createdAt', value: moment('20180101').toDate() }] }).find().then(productReturns => {
+      return ReturnsModel.getReturnsByFilters({ limit: 100000 }).find().then(productReturns => {
         console.log('done return')
         return Promise.all(products.map(product => this.createProductStatsObject(product, null, productReturns)))
       })
@@ -27,13 +27,14 @@ exports.StatsController = new class StatsCrontroller {
   }
 
   createProductStatsObject(productObject, productOrders, productReturns) {
-    const unitsRepaired = typeof productObject.get('totalRepaired') !== 'undefined' ? productObject.get('totalRepaired') : 0;
     var countingSold = 0;
-    var countingReturn = productReturns.filter(result => result.get('productId') == productObject.get('productId')).length;
+    var filteredReturns = productReturns.filter(result => result.get('productId') == productObject.get('productId'));
+    var countingReturn = filteredReturns.filter(result => result.get('returnTypeId') == 0).length;
+    var countingRepair = filteredReturns.filter(result => result.get('returnTypeId') == 1).length;
     // _.filter(productOrders,result => result.get('product_id') == productObject.get('productId')).map(result => countingSold += result.get('quantity'))
 
     return OrdersModel.getOrderProductsByFilters(
-      { limit: 10000, equal: [{ key: 'product_id', value: productObject.get('productId') }], greaterOrEqual: [{ key: 'createdAt', value: moment('20180101').toDate() }] }).find().then(productOrders => {
+      { limit: 10000, equal: [{ key: 'product_id', value: productObject.get('productId') }] }).find().then(productOrders => {
         productOrders.map(result => countingSold += result.get('quantity'))
         return {
           productId: productObject.get('productId'),
@@ -41,8 +42,8 @@ exports.StatsController = new class StatsCrontroller {
           totalSold: countingSold,
           unitsReturned: countingReturn,
           unitsReturnedP: countingSold !== 0 ? (countingReturn * 100) / countingSold : 0,
-          unitsRepaired,
-          unitsRepairedP: countingSold !== 0 ? (unitsRepaired * 100) / countingSold : 0,
+          unitsRepaired: countingRepair,
+          unitsRepairedP: countingSold !== 0 ? (countingRepair * 100) / countingSold : 0,
           totalReveneu: productObject.get('price') * countingSold
         };
       })
